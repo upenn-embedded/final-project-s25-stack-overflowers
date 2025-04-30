109ad477f3c1739bf233a9e92553395b16275032
amandagarbel <amandatg@seas.upenn.edu>
2025-04-30 12:57:52 -0400

    blynk code

--- blynkCode.cpp
+++ blynkCode.cpp
@@ -1,51 +1,56 @@
-
+/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID           "TMPL2AbcmmxM2" 
+
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+#define BLYNK_TEMPLATE_ID           "TMPL2AbcmmxM2"
 #define BLYNK_TEMPLATE_NAME         "ESE3500"
 #define BLYNK_AUTH_TOKEN            "GbFkys4_FPw8G8-EQ9veBrG9TAoxZ7Q4"
 
-#define MY_SSID                     "AirPennNet-Device" 
-#define MY_PASSWORD                 "penn1740wifi" 
+// Your WiFi credentials.
+// Set password to "" for open networks.
+#define MY_SSID                     "AirPennNet-Device"
+#define MY_PASSWORD                 "penn1740wifi"
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
 char auth[] = BLYNK_AUTH_TOKEN;
-#define CONTROL_PIN 17
 
+// Define UART pins for communication with ATMEGA
+#define UART_TX_PIN 17
+#define UART_RX_PIN 18  
 
 void setup()
 {
-  Serial.begin(9600);
+  // Debug console to USB serial
+  Serial.begin(115200);
+  
+  // UART communication with ATMEGA
+  Serial1.begin(9600, SERIAL_8N1, UART_RX_PIN, UART_TX_PIN);
+  
+  // Connect to Blynk
   Blynk.begin(auth, MY_SSID, MY_PASSWORD);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(CONTROL_PIN, OUTPUT);
+  
+  Serial.println("ESP32 initialized and connected to Blynk");
 }
 
 void loop()
 {
-  Blynk.run();                
-    
-}
-
-BLYNK_WRITE(V1)
-{
-  if(param.asInt() == 1)
-  {
-    digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(CONTROL_PIN, HIGH);
-  }
-  else
-  {
-   digitalWrite(LED_BUILTIN, LOW);
-   digitalWrite(CONTROL_PIN, LOW); 
+  Blynk.run();
+  
+  // Forward serial data from ATMEGA to debug console
+  while (Serial1.available()) {
+    char c = Serial1.read();
+    Serial.write(c);  
   }
 }
 
 BLYNK_WRITE(V0)
 {
-  String inputText = param.asStr();  // get input from app
-  Serial.println("Received from Blynk: " + inputText);
-  Serial1.println(inputText);  //send to atmega
+  String inputText = param.asStr();  // Get text input
+  Serial.println("Sending to ATMEGA: " + inputText);  
+  Serial1.println(inputText);  // Send to ATmega via UART
 }
